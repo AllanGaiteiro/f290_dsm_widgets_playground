9994044deaa738fd65e4e4d61525470e2381ce21
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-12 09:48:34 -0300

    faker adciona um nome aleatorio

--- lib/pages/cotacao_moedas_page.dart
+++ lib/pages/cotacao_moedas_page.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 class CotacaoMoedasPage extends StatefulWidget {
@@ -8,13 +9,15 @@ class CotacaoMoedasPage extends StatefulWidget {
 }
 
 class _CotacaoMoedasPagesState extends State<CotacaoMoedasPage> {
+  final faker = Faker();
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: 10, // numero de elementos gerados
       itemBuilder: (context, index) {
         return Card(
-          child: ListTile(title: Text("Titulo do Card ${index + 1}")),
+          child: ListTile(title: Text(faker.person.name())),
         );
       },
     );
